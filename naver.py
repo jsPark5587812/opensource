21b3659b03d23dc0029985139d81eec66a7e083f
jsPark <jspark55871@gmail.com>
2024-12-07 16:28:38 +0900

    new link, source added

--- naver.py
+++ naver.py
@@ -2,27 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# 키워드와 URL 설정
+keyword = '비트코인'
+url = f'https://search.naver.com/search.naver?where=news&query={keyword}'
 
-def start():
-    # 키워드, URL
-    keyword = '비트코인'
-    url = f'https://search.naver.com/search.naver?where=news&query={keyword}'
+# 웹 페이지 요청 및 파싱
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 웹 페이지 요청
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+# 뉴스 제목, 링크, 매체명 추출
+articles = soup.select('.news_tit')
+press_names = soup.select('.info_group .press')  # 뉴스 매체 이름 추출
 
-    # news_tit 부분 필터링
-    articles = soup.select('.news_tit')
-    print( articles )
+# 데이터 수집
+news_data = [
+    {
+        'title': article.text,
+        'link': article['href'],
+        'press': press.text if press else 'Unknown'
+    }
+    for article, press in zip(articles, press_names)
+]
 
-    # 뉴스 제목 확인
-    article = articles[0]
-    title = article.text
-    print ( title )
-    # 뉴스 제목 데이터프레임 생성
-    title_tot = pd.DataFrame([article.text for article in articles], columns=['title'])
-    print( title_tot.head())
+# 데이터프레임 생성
+df_news = pd.DataFrame(news_data)
 
+# 데이터 출력 (최상위 5개)
+print(df_news.head())
 
-start() 
